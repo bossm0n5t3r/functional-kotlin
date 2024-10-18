a9fd8cf0ded14203fa1969492f4024b84a3f20c0
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-10-19 01:30:00 +0900

    :tada:

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+}
+
+group = "functional"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+ktlint {
+    version.set("1.3.1")
+}
